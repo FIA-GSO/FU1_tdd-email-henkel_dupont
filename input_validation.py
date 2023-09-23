688ca7fcc17c44c3af38d02c7ee848e2e2e62561
Jerome Dupont <jerome.d3@gso.schule.koeln>
2023-09-23 10:54:28 +0200

    formatting makes life better

--- input_validation.py
+++ input_validation.py
@@ -1,7 +1,25 @@
 import re  # regular Expressions
 
 validSpecialCharacters = [
-    '!', '#', '$', '%', '&', "'", '*', '+', '-', '/', '=', '?', '^', '_', '`', '{', '|', '}', '~'
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "*",
+    "+",
+    "-",
+    "/",
+    "=",
+    "?",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
 ]
 
 
@@ -10,74 +28,84 @@ def is_valid_email(email: str) -> bool:
     Uses regular expression to validate email.
 
     Returns:
-    True -- email is valid 
+    True -- email is valid
     False -- email is not valid
     """
 
-    if(type(email) != str):
+    if type(email) != str:
         return False
-    if(not email or email == ""):
+    if not email or email == "":
         return False
-    splitAt = email.split('@')
-    if(len(splitAt) != 2):
+    splitAt = email.split("@")
+    if len(splitAt) != 2:
         return False
-    if(len(splitAt[0]) == 0):
+    if len(splitAt[0]) == 0:
         return False
     firstChar = splitAt[0][0]
-    if(not firstChar.isalnum() and firstChar != '"'):
+    if not firstChar.isalnum() and firstChar != '"':
         return False
     lastChar = splitAt[0][-1]
-    if(not lastChar.isalnum() and lastChar != '"'):
+    if not lastChar.isalnum() and lastChar != '"':
         return False
-    splitDot = splitAt[1].split('.')
-    if(len(splitDot) < 2):
+    splitDot = splitAt[1].split(".")
+    if len(splitDot) < 2:
         return False
 
     for character in splitAt[0]:
         isAlnum = character.isalnum()
-        isDot = character == '.'
+        isDot = character == "."
         isSpecialAllowed = validSpecialCharacters.__contains__(character)
         valid = isAlnum or isDot or isSpecialAllowed
-        if (not valid):
+        if not valid:
             return False
 
     # DOMAINS BEFORE DOT
     for character in splitDot[0]:
         isAlnum = character.isalnum()
-        isDot = character == '.'
+        isDot = character == "."
         isSpecialAllowed = validSpecialCharacters.__contains__(character)
         valid = isAlnum or isDot or isSpecialAllowed
-        if (not valid):
+        if not valid:
             return False
 
     # AFTER DOT
     for character in splitDot[1:]:
         isAlpha = character.isalpha()
-        isDot = character == '.'
+        isDot = character == "."
         valid = isAlpha or isDot
-        if (not valid):
+        if not valid:
             return False
     return True
 
 
 def is_valid_password(password: str):
-    if(type(password) is not str): return TypeError
+    if type(password) is not str:
+        return TypeError
     length = len(password)
-    if(length < 8): return False
+    if length < 8:
+        return False
     typesOfCharacters = get_types_of_characters_used(password)
-    if(typesOfCharacters < 2): return False
-    if(typesOfCharacters == 2 and length < 20): return False
-    if(typesOfCharacters == 3 and length < 13): return False
+    if typesOfCharacters < 2:
+        return False
+    if typesOfCharacters == 2 and length < 20:
+        return False
+    if typesOfCharacters == 3 and length < 13:
+        return False
     return True
 
 
 def get_types_of_characters_used(password: str) -> int:
-    types = {"isLower": False, "isUpper": False, "isDigit": False, "isSpecial": False}
+    types = {"isLower": False, "isUpper": False,
+             "isDigit": False, "isSpecial": False}
     for character in password:
-        if(character.isalpha() and character.isupper()): types["isUpper"] = True
-        elif(character.isalpha() and character.islower()): types["isLower"] = True
-        elif(character.isdigit()): types["isDigit"] = True
-        else: types["isSpecial"] = True
+        if character.isalpha() and character.isupper():
+            types["isUpper"] = True
+        elif character.isalpha() and character.islower():
+            types["isLower"] = True
+        elif character.isdigit():
+            types["isDigit"] = True
+        else:
+            types["isSpecial"] = True
     return sum(types.values())
 
 
